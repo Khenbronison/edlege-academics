9b61e32b8004bc87150415465ab99945e10a3ae5
KhenBronison <amoahken1999@gmail.com>
2025-05-22 20:28:28 -1200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-red-300">Edlege Academics</h1>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
