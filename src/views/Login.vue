90d0f5b03a94be3bd964935575cbb86d0be3f1b1
Godfred Nai <godfrednai@gmail.com>
2025-05-23 14:19:22 +0000

    login

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="h-screen bg-gray-100 flex justify-center items-center">
+    <form
+      class="p-4 border border-black shadow-md rounded-md flex flex-col w-full md:max-w-[300px]"
+    >
+      <h1 class="text-center">Edlege Login</h1>
+      <div>
+        <p class="text-center">Please enter your code here</p>
+      </div>
+      <div>
+        <input type="text" class="w-full bg-transparent border border-black rounded-md p-1" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const loading = ref(false);
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+    "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx,vue}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
